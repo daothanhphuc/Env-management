818413cf378b4145572e64cc7e2e597b80a0bf5b
daothanhphuc <fuc25032004@gmail.com>
2024-12-02 21:14:42 +0700

    new functions

--- router/add.py
+++ router/add.py
@@ -1,20 +1,29 @@
 from fastapi import APIRouter, Depends
 from db.schemas import UserBase, RoleDisplay, LFBase, LFDisplay, constructionBase, constructionDisplay
-from db.schemas import processingBase, processingDisplay
+from db.schemas import processingBase, processingDisplay, HanhChinhBase, HanhChinhDisplay, documentBase, documentDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
-from db import guest_permission, admin_permission
-from db import livestock, construction, product
+from db import guest_permission, admin_permission, huyen_xa_moi
+from db import livestock, construction, product, legal_docs
 router = APIRouter(
     prefix="/add",
     tags=["add"]
 )
+# Huyen,xa
+@router.post("/add_huyen", response_model = HanhChinhDisplay)
+def add_huyen ( db: Session=Depends(get_db)):
+    return huyen_xa_moi.huyen(db,request)
+@router.post("/add_xa", response_model = HanhChinhDisplay)
+def add_xa ( db: Session = Depends(get_db)):
+    return huyen_xa_moi.xa(db,request)
+
 @router.post("/add_guest_role", response_model=RoleDisplay)
 def add_guest_role(db: Session=Depends(get_db)):
     return guest_permission.create_guest_role(db)
 @router.post("/add_admin_role", response_model=RoleDisplay)
 def add_admin_role(db: Session=Depends(get_db)):
     return admin_permission.create_admin_role(db)
+
 #Livestock
 @router.post("/add_LF", response_model=LFDisplay)
 def add_LF(request: LFBase, db: Session=Depends(get_db)):
@@ -38,3 +47,6 @@ def add_processing_facilities(request: processingBase, db: Session=Depends(get_d
     return product.new_facilities(db, request)
 
 # Legal document
+@router.post("/add_docs", response_model = documentDisplay)
+def add_legal_doc (request: documentBase, db: Session=Depends(get_db)):
+    return legal_docs.new_legal_doc(db, request)

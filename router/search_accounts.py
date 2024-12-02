25b79d745035a2555a85c7fe1042cbec72784ca6
daothanhphuc <fuc25032004@gmail.com>
2024-12-02 21:56:30 +0700

    update prefix and post

--- router/search_accounts.py
+++ router/search_accounts.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import Session
 from db.database import get_db
 from db import admin_permission
 router = APIRouter(
-    prefix="/search_accounts",
-    tags=["search_accounts"]
+    prefix="/search",
+    tags=["search"]
 )
-@router.post("/search_guest_accounts", response_model=UserDisplay)
+@router.post("/users/", response_model=UserDisplay)
 def search_guest_accounts(find_id: int, db: Session=Depends(get_db)):
     return admin_permission.search_guest_accounts(db=db, find_id=find_id)
+def get_all_users(db: Session=Depends(get_db)):
+    return admin_permission.get_all(db=db)

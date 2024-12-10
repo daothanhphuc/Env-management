0b1b60e6b1de492abf41cf205431c6fc0b948354
daothanhphuc <fuc25032004@gmail.com>
2024-12-10 21:44:15 +0700

    comment a few lines

--- router/search_accounts.py
+++ router/search_accounts.py
@@ -7,8 +7,10 @@ router = APIRouter(
     prefix="/search",
     tags=["search"]
 )
-@router.post("/users/", response_model=UserDisplay)
-def search_guest_accounts(find_id: int, db: Session=Depends(get_db)):
-    return admin_permission.search_guest_accounts(db=db, find_id=find_id)
-def get_all_users(db: Session=Depends(get_db)):
-    return admin_permission.get_all(db=db)
+@router.post("/all/", response_model=list[UserDisplay])
+# def search_guest_accounts(find_id: int, db: Session=Depends(get_db)):
+#     return admin_permission.search_guest_accounts(db=db, find_id=find_id)
+async def get_all_users(db: Session=Depends(get_db)):
+    # Fetch the list of dbUser objects from the database
+    users = admin_permission.get_all(db=db, request=UserBase)
+    return [UserDisplay.from_orm(user) for user in users]
